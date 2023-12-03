e76ca608ad69be268133d7d80f4663b6847c4062
Bowie <andre_luiz29@hotmail.com>
2023-12-03 20:50:20 -0300

    templates

---
+++ app_instalacoes/metragem.py
@@ -0,0 +1,37 @@
+# Constantes globais
+VALOR_UNITARIO_1 = 38.5
+VALOR_UNITARIO_2 = 18.59
+VALOR_UNITARIO_3 = 20.69
+VALOR_UNITARIO_4 = 22.89
+VALOR_UNITARIO_5 = 21.97
+
+
+def metragens(metragem):
+    if metragem < 1.99:
+        valor_unitario = VALOR_UNITARIO_1
+        valor_total = VALOR_UNITARIO_1
+        return metragem, valor_unitario, valor_total
+    elif metragem <= 2.49:
+        valor_unitario = VALOR_UNITARIO_2
+    elif metragem <= 2.99:
+        valor_unitario = VALOR_UNITARIO_3
+    else:
+        valor_unitario = VALOR_UNITARIO_4
+
+    valor_total = metragem * valor_unitario
+    return metragem, valor_unitario, valor_total
+
+def metragem_adicionadas(metragem):
+    valor_unitario = VALOR_UNITARIO_2
+    valor_total = metragem * valor_unitario
+    return metragem, valor_unitario, valor_total
+
+def metragem_taxa(taxa):
+    metragem = 0
+    valor_unitario = taxa
+    valor_total = taxa
+    return metragem, valor_unitario, valor_total
+
+def wave(metragem):
+    valor_total = metragem * VALOR_UNITARIO_5
+    return metragem, VALOR_UNITARIO_5, valor_total
--- app_instalacoes/views.py
+++ app_instalacoes/views.py
@@ -1,11 +1,24 @@
 from django.shortcuts import render
-from .models import Instalacao
+from django.http import HttpResponse
+from .metragem import *
 
 # Create your views here.
 def home(request):
     return render(request,'home.html')
 
 def pedido_cadastrados(request):
-    nova_instalacao = Instalacao()
-    nova_instalacao.pedido = request.Post.get('pedido')
-    nova_instalacao.vendedor = request.Post.get('vendedor')
+    if request.method == "GET":
+        return render(request, 'home.html')
+    elif request.method == "POST":
+        pedido = request.POST.get('pedido')
+        vendedor = request.POST.get('vendedor')
+        valores = request.POST.get('metragem_inteiro')
+        if valores != 0:
+            valores = list(metragens(float(valores)))
+            for valor in valores:
+                metragem = valores[0]
+                valor_unitario = valores[1]
+                valor_total = valores[2]
+
+        print(f'Pedido: {pedido} Vendedor: {vendedor} metragem: {metragem} valor unitario: {valor_unitario} valor Total: {valor_total} ')
+        return  HttpResponse("Estou Aqui!")

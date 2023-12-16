60285fbd2d39bdbe8cc1772dfb680cb1f9498c31
Bowie <andre_luiz29@hotmail.com>
2023-12-15 21:06:56 -0300

    cadastro.html

--- app_instalacoes/views.py
+++ app_instalacoes/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .metragem import *
+from .metragem import metragem_taxa, metragem_adicionadas, metragens, wave
 from .models import Instalacao
 
 # Create your views here.
@@ -13,44 +13,58 @@ def pedido_cadastrados(request):
     elif request.method == "POST":
         pedido = request.POST.get('pedido')
         vendedor = request.POST.get('vendedor')
-        valores = request.POST.get('metragem_inteiro')
-        if valores != 0:
-            valores = list(metragens(float(valores)))
-            for valor in valores:
-                metragem = valores[0]
-                valor_unitario = valores[1]
-                valor_total = valores[2]
-        valores = request.POST.get('metragem_fracionaro')
-        if valores != 0:
-            valores = list(metragem_adicionadas(float(valores)))
-            for valor in valores:
-                metragem = valores[0]
-                valor_unitario = valores[1]
-                valor_total = valores[2]
-        valores = request.POST.get('metragem_wave')
-        if valores != 0:
-            valores = list(wave(float(valores)))
-            for valor in valores:
-                metragem = valores[0]
-                valor_unitario = valores[1]
-                valor_total = valores[2]
-        valores = request.POST.get('taxa')
-        if valores != 0:
-            valores = list(metragem_taxa(float(valores)))
-            for valor in valores:
-                metragem = valores[0]
-                valor_unitario = valores[1]
-                valor_total = valores[2]
+
+        valores_inteiro = request.POST.get('metragem_inteiro')
+        if valores_inteiro != '0':
+            valores_inteiro = list(metragens(float(valores_inteiro)))
+            for valor in valores_inteiro:
+                metragem = valores_inteiro[0]
+                valor_unitario = valores_inteiro[1]
+                valor_total = valores_inteiro[2]
+
+        metragem_fracionaro = request.POST.get('metragem_fracionaro')
+        if metragem_fracionaro != '0':
+            metragem_fracionaro = list(metragem_adicionadas(float(metragem_fracionaro)))
+            for valor in metragem_fracionaro:
+                metragem = metragem_fracionaro[0]
+                valor_unitario = metragem_fracionaro[1]
+                valor_total = metragem_fracionaro[2]
+
+        metragem_wave = request.POST.get('metragem_wave')
+        if metragem_wave != '0':
+            metragem_wave = list(wave(float(metragem_wave)))
+            for valor in metragem_wave:
+                metragem = metragem_wave[0]
+                valor_unitario = metragem_wave[1]
+                valor_total = metragem_wave[2]
+
+        taxa = request.POST.get('taxa')
+        if taxa != '0':
+            taxa = list(metragem_taxa(float(taxa)))
+            for valor in taxa:
+                metragem = taxa[0]
+                valor_unitario = taxa[1]
+                valor_total = taxa[2]
 
         # Salvar instalação no Banco de dados
-        cadastro_instalacao = Instalacao()
-        cadastro_instalacao.pedido = pedido
-        cadastro_instalacao.vendedor = vendedor
-        cadastro_instalacao.metragem = metragem
-        cadastro_instalacao.valor_unitario = valor_unitario
-        cadastro_instalacao.valor_total = valor_total
+        cadastro_instalacao = Instalacao(
+            pedido=pedido,
+            vendedor=vendedor,
+            metragem=metragem,
+            valor_unitario=valor_unitario,
+            valor_total=valor_total
+        )
+
         cadastro_instalacao.save()
 
-        # Exibir todos as instalação em nova pagina
 
-        return  HttpResponse("Estou Aqui!")
+
+
+
+        # Exibir todos as instalação em nova pagina
+        instalacoes_cadastradas = {
+            'cadastros': Instalacao.objects.all()
+        }
+
+        
+        return render(request, 'cadastros.html', instalacoes_cadastradas)
